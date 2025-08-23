96c3b5df1b1219b77115158f2ee3b0d04f508d1d
U-oeee <delta7721@naver.com>
2025-08-23 16:28:57 +0900

    Add files via upload

---
+++ rplidar_a2m8_launch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    channel_type = LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+    frame_id = LaunchConfiguration('frame_id', default='front_laser_link')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    scan_mode = LaunchConfiguration('scan_mode', default='Standard')
+    dtr_control = LaunchConfiguration('dtr_control', default='false')
+
+    return LaunchDescription([
+        DeclareLaunchArgument('channel_type', default_value=channel_type),
+        DeclareLaunchArgument('serial_port', default_value=serial_port),
+        DeclareLaunchArgument('serial_baudrate', default_value=serial_baudrate),
+        DeclareLaunchArgument('frame_id', default_value=frame_id),
+        DeclareLaunchArgument('inverted', default_value=inverted),
+        DeclareLaunchArgument('angle_compensate', default_value=angle_compensate),
+        DeclareLaunchArgument('scan_mode', default_value=scan_mode),
+        DeclareLaunchArgument('dtr_control', default_value=dtr_control),
+
+        Node(
+            package='rplidar_ros',
+            node_executable='rplidar_node',        # ✅ 반드시 node_executable
+            node_name='rplidar_node',              # ✅ 반드시 node_name
+            parameters=[{
+                'channel_type': channel_type,
+                'serial_port': serial_port,
+                'serial_baudrate': serial_baudrate,
+                'frame_id': frame_id,
+                'inverted': inverted,
+                'angle_compensate': angle_compensate,
+                'scan_mode': scan_mode,
+                'dtr_control': dtr_control
+            }],
+            output='screen'
+        )
+    ])
+
